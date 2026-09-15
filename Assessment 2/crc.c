9af73fcab128dd87237d1f7f826c53ea6a06cf1a
AS-Nath <aditya.nath.tech@gmail.com>
2026-09-15 20:33:05 +0530

    CRC

--- Assessment 2/crc.c
+++ Assessment 2/crc.c
@@ -2,6 +2,59 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+int locate_msb(uint64_t n) {
+    int p = 0; 
+    while (n > 1) {
+        p += 1; 
+        n >>= 1; 
+    }
+    return p; 
+}
+
+uint16_t generate_remainder(uint64_t message, uint32_t generator) {
+    int msb = locate_msb(message); 
+    int window = msb - 16; 
+    uint64_t temp = message >> window; 
+    while (window > 0) {
+        if (message & (1ULL << msb)) {
+            temp ^= generator;
+        }
+        msb -= 1;
+        window -= 1; 
+        temp = ((temp << 1) | ((message >> window) & 1));
+    }
+    return (uint16_t)temp; 
+}
+
+int receive(uint64_t message, uint32_t generator) {
+    uint16_t rem = generate_remainder(message, generator); 
+    return !rem;
+}
+
+uint64_t transmit(uint64_t message, uint32_t generator) {
+    message |= generate_remainder(message, generator);
+    return message; 
+}
+
+void corrupt(uint64_t* n, int bit) {
+    uint64_t t = *n;
+    t ^= (1ULL << bit); 
+    *n = t;  
+}
+
 int main(void) {
-    
+    uint64_t message; 
+    uint32_t generator; 
+    scanf("%" SCNu64, &message); 
+    scanf("%" SCNu32, &generator); 
+    message <<= 16; // CRC-16 code
+    message = transmit(message, generator);  
+    //corrupt(&message, 2); 
+    int status = receive(message, generator); 
+    if (status) {
+        printf("Received!\n"); 
+    }
+    else {
+        printf("Error detected!\n"); 
+    }
 }
